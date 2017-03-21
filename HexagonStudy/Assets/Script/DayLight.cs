4667dbae393b00739b4e1662cc6543922374e54e
AGM-FRAN\Flan <resetme@gmail.com>
2017-03-21 14:42:56 +0900

    Files

---
+++ HexagonStudy/Assets/Script/DayLight.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayLight : MonoBehaviour {
+    [SerializeField]
+    private Transform _directionalLight;
+    [SerializeField]
+    private HexagonScript _hexagonScript;
+    [SerializeField]
+    private Transform _cameraPivot;
+
+    private float lightpos;
+    private bool night = false;
+
+    private Coroutine _routine;
+    private bool _routineStart = false;
+
+	void Update () {
+        _directionalLight.Rotate(0.25f,0,0);
+        _cameraPivot.Rotate(0, 0.05f, 0);
+        lightpos = Mathf.Abs(_directionalLight.localRotation.x);
+
+       if ( lightpos > 0.9f && !night) {
+            //Night
+            night = true;
+            if (_routineStart)
+                StopCoroutine(_routine);
+
+            _routine = StartCoroutine(DayLightTransition());
+        }
+
+        if (lightpos < 0.1f && night) {
+            //Day
+            night = false;
+            if (_routineStart)
+                StopCoroutine(_routine);
+
+            _routine = StartCoroutine(DayLightTransition());
+        }
+
+    }
+
+    IEnumerator DayLightTransition() {
+        _routineStart = true;
+
+        float t=0;
+        int nightWave = 2;
+        int dayWave = 20;
+        float curWave = _hexagonScript.wave;
+        float toWave;
+
+        int seconds = 4;
+
+        if (night)
+        {
+            toWave = nightWave;
+        }
+        else {
+            toWave = dayWave;
+        }
+
+        while (t < seconds) {
+            t += Time.deltaTime;
+            _hexagonScript.wave = Mathf.Lerp(curWave, toWave, Mathf.SmoothStep(0,1, t / seconds));
+            yield return null;
+        }
+        _routineStart = false;
+
+    }
+}
---
+++ HexagonStudy/Assets/Script/HexagonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonScript : MonoBehaviour {
+    //[SerializeField]
+    [Range(1,20)]
+    public float wave = 1;
+
+    private Transform[] _hexagonRT;
+    private int _hexagonsCount;
+
+    private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        _hexagonsCount = transform.childCount;
+        _hexagonRT = new Transform[_hexagonsCount];
+
+        for (int i = 0; i < _hexagonsCount; i++)
+        {
+            _hexagonRT[i] = transform.GetChild(i).GetComponent<Transform>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        for (int i = 0; i < _hexagonsCount; i++)
+        {
+            pos = _hexagonRT[i].position;
+            pos.y = Mathf.Sin(Time.time+i)/wave;
+            _hexagonRT[i].position = pos;
+        }
+	}
+}
